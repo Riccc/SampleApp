8541fefcc964c134f3e0a8338fe71af25bcd3f98
唐民鹏 <minpeng.tang@hand-china.com>
2016-09-13 14:57:39 +0800

    xxx

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "valid signup information" do
+    get signup_path
+    name     = "Example User"
+    email    = "user@example.com"
+    password = "password"
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  name,
+                                            email: email,
+                                            password:              password,
+                                            password_confirmation: password }
+    end
+    assert_template 'users/show'
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -13,5 +27,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
 end
